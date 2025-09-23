b6d2c8807a3ba9a2b17df05375755fe6564720d7
itsmeoussama <bekkali.oussama.solicode@gmail.com>
2025-09-23 11:02:49 +0100

    removed some folders

--- Exercice 2/script.js
+++
@@ -1,9 +0,0 @@
-let note = prompt("enter votre note");
-
-note = Number(note);
-
-let message = (note < 10) ? "insuffisant" :
-              (note <= 12) ? "passable" :
-              "bien";
-
-alert(message);
--- exercice 1/script.js
+++
@@ -1,7 +0,0 @@
-let pw = prompt("enter the pw");
-
-if(pw=="user@123"){
-    alert("Acces autorise")
-}else{
-    alert("Accès refusé")
-}
