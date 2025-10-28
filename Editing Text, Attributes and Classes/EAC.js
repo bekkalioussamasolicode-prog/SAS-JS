71aeea5e814b638378a2e781ce3e5879002de337
itsmeoussama <bekkali.oussama.solicode@gmail.com>
2025-10-28 15:59:09 +0000

    add  EAC

--- Editing Text, Attributes and Classes/EAC.js
+++ Editing Text, Attributes and Classes/EAC.js
@@ -5,6 +5,9 @@ let bouton = document.querySelector(".btn");
 bouton.addEventListener("click", function () {
   titre.innerText = "Titre modifié !";
   titre.classList.toggle("highlight");
-  image.setAttribute("src", "photo.png");
+  image.setAttribute(
+    "src",
+    "https://images.pexels.com/photos/250591/pexels-photo-250591.jpeg"
+  );
   image.setAttribute("alt", "Nouvelle image");
 });
